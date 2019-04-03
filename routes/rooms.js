2d527caf5eae24f6ed719b120f120b12f4edbabe
bynjamin <filip.konig@vacuumlabs.com>
2019-04-03 11:34:13 +0200

    Slack request auth middleware

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const {getRoomsInfoRaw} = require('../src/google')
 const {printRoomsInfo} = require('../src/slack')
+const authenticateRequest = require('../middleware/slackAuthMiddleware')
 
 const router = express.Router()
 
-router.get('/rooms/raw', async (req, res) => {
-  res.send(await getRoomsInfoRaw('BA'))
-})
-router.post('/rooms', async (req, res) => {
-  const office = req.body.text.trim().toUpperCase()
-  res.status(200).send(await printRoomsInfo(office))
-})
+router
+  .use(authenticateRequest)
+  .get('/rooms/raw', async (req, res) => {
+    res.send(await getRoomsInfoRaw('BA'))
+  })
+  .post('/rooms', async (req, res) => {
+    const office = req.body.text.trim().toUpperCase()
+    res.status(200).send(await printRoomsInfo(office))
+  })
 
 module.exports = router
