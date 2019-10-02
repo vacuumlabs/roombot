70e6d141691cf09592bcf45ed2fa561730f41c2e
bynjamin <filip.konig@vacuumlabs.com>
2019-10-02 15:09:21 +0200

    Add delayed response functionality
    Google API somethimes takes more than 3 sec. which is slack slack's
    default slash command timeout, so we had to implement delayed response.
    See https://api.slack.com/slash-commands#responding_response_url

--- routes/rooms.js
+++ routes/rooms.js
@@ -1,5 +1,5 @@
 const express = require('express')
-//const axios = require('axios')
+const axios = require('axios')
 const {printRoomsInfo} = require('../src/slack')
 const authenticateRequest = require('../middleware/slackAuthMiddleware')
 
@@ -9,15 +9,17 @@ router.use(authenticateRequest)
 
 router.post('/rooms', async (req, res) => {
   const office = req.body.text.trim().toUpperCase()
-  //const delayedResponeUrl = req.body.response_url
-  res.status(200).send(await printRoomsInfo(office))
-/*
+  const delayedResponeUrl = req.body.response_url
+
+  // Imidiate response (timeout = 3 sec.)
+  res.status(200).send('Roombot is processing your request...')
+
+  // Delayed response (google API calls > 3s)
   const responseData = await printRoomsInfo(office)
   axios.post(delayedResponeUrl, responseData)
     .catch((error) => {
       console.log(error.response)
     })
-    */
 })
 
 module.exports = router
